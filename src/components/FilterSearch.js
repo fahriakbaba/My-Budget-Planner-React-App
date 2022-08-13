51bf4fe2c1d654174fa7b0dfb513475c36d86797
Fahri Akbaba <fahriakbaba@gmail.com>
2022-08-13 19:12:28 +0300

    display items from redux-toolkit

--- src/components/FilterSearch.js
+++ src/components/FilterSearch.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {RiDeleteBin5Line} from 'react-icons/ri';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import { useSelector } from "react-redux";
 
 function FilterSearch() {
-  return(
+  const { items } = useSelector(state => state.budget);
+  console.log(items);
+  return (
     <article>
       <input type="text" className='form-control' placeholder='Type to search...' />
       <ul className='list-group mt-3'>
-        <li className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'>
-          budget list item
-          <span className='ms-auto badge bg-primary'>$66</span>
-          <RiDeleteBin5Line className='text-danger fs-5 ms-2 hover' style={{cursor: "pointer",}} />
-        </li>
+        {items.map(item => {
+          return (
+            <li className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'>
+              {item.title}
+              <span className='ms-auto badge bg-primary'>${item.price}</span>
+              <RiDeleteBin5Line className='text-danger fs-5 ms-2 hover' style={{ cursor: "pointer", }} />
+            </li>
+          )
+        })}
       </ul>
     </article>
   )
 }
 
 export default FilterSearch;
+
+
+
